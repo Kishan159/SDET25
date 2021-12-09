172def1e6f8ffde6ceb93bd3f72e1383624c0796
Admin <Admin@DESKTOP-C8F0LTH>
2021-12-09 19:11:44 +0530

    Edited ,kishore

--- src/test/java/SDET1.java
+++ src/test/java/SDET1.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 public class SDET1 {
 	@Test
 	public void DateTest() {
-		System.out.println("GitHub Repository");
+		System.out.println("GitHub Repository Kishore");
 		System.out.println("GithubRepository edited ");
 		System.out.println("GithubRepository25");
 		System.out.println("GitRepository");
